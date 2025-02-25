e69fc0a862c68c11346d48849956609f716d2230
Djole <0xdjole@gmail.com>
2025-02-25 19:53:16 +0100

    fix eslint

--- src/wordpress/index.ts
+++ src/wordpress/index.ts
@@ -1,3 +1,3 @@
-export * as types from "./types.js";
-export * as api from "./api.js";
-export * as components from "./components/index.js";
+export * as types from './types.js';
+export * as api from './api.js';
+export * as components from './components/index.js';
